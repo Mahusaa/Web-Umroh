688854f4f048ffa001386e87add1d35ccbfdbe0c
Mahusaa <usamah.hafizh@ui.ac.id>
2024-09-06 15:33:03 +0700

    feat: add create new Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import { Button } from "~/components/ui/button";
+import Link from "next/link";
 
 export default function HomePage() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <Button className="rounded">Buat Baru</Button>
+      <Link href={"/create-new"}>
+        <Button className="rounded">Buat Baru</Button>
+      </Link>
     </div>
   )
 }
